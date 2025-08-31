1432bc16c78a168742a60168d0ee33f113bd9e02
Muhammad Masood Hussain <144346171+Masood-Hussain@users.noreply.github.com>
2025-08-31 18:35:16 +0500

    OrderStatus.java

---
+++  Microservice-based Food Delivery System/common-lib/src/main/java/com/fooddelivery/common/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.fooddelivery.common.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    PREPARING,
+    READY_FOR_PICKUP,
+    PICKED_UP,
+    DELIVERED,
+    CANCELLED
+}
